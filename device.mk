ef412e1b742e7e6b589961fe1a22ef39c79b8d4a
Alexandru Scurtu <sasha.scurtu@gmail.com>
2024-04-21 17:18:59 +0200

    ruby: Build sensorservice 1.0 HAL
    W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    W libc    : Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20
    
    E init    : Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 530 (/system/bin/hwservicemanager)
    
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- device.mk
+++ device.mk
@@ -399,6 +399,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.frameworks.sensorservice@1.0.vendor \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@2.1-service.xiaomi-multihal
 
 PRODUCT_PACKAGES += \
