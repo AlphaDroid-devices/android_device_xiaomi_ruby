4c1409b8e0787ed0a9df502cd9877ba06c4bbf51
Allien10X <erdemiryz38@gmail.com>
2024-06-19 02:47:40 +0300

    ruby: Import Dolby blobs from stock
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- device.mk
+++ device.mk
@@ -56,6 +56,9 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Audio
+TARGET_EXCLUDES_AUDIOFX := true
+
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
