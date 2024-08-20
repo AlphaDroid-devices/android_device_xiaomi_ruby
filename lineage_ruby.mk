dbea41908b3ec4c24456c5168c0e2db800b61e6c
Allien10X <erdemiryz38@gmail.com>
2024-08-20 09:18:25 -0700

    ruby: Welcome @AlphaDroid-Project
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- lineage_ruby.mk
+++ lineage_ruby.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/ruby/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common AlphaDroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_SCREEN_DENSITY := 440
@@ -29,3 +29,19 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="ruby-user 14 UP1A.230620.001 V816.0.4.0.UMOMIXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/ruby_global/ruby:14/UP1A.230620.001/V816.0.4.0.UMOMIXM:user/release-keys
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BUILD_PACKAGE := 1
+TARGET_LAUNCHER := 2
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Allien10X
